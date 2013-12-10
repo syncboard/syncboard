79cc1c319d3c29c80b20d9c549af98c81e4374da
Nat Mote <nat@natmote.net>
2013-12-09 21:06:58 -0800

    fix typo in license

--- src/gui_status.py
+++ src/gui_status.py
@@ -1,5 +1,5 @@
 """
-    Cross-platform clipboard sycning tool
+    Cross-platform clipboard syncing tool
     Copyright (C) 2013  Syncboard
 
     This program is free software; you can redistribute it and/or modify
